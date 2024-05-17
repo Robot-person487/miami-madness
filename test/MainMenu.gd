02d120568431cfb4767f1901606d7de722f409e8
Dylan Bauer <pickleeyesmc@gmail.com>
2024-05-17 14:18:42 -0400

    Add files via upload

---
+++ test/MainMenu.gd
@@ -0,0 +1,12 @@
+extends Node2D
+
+var text : String = "hello"
+var number = 23
+# Called when the node enters the scene tree for the first time.
+func _ready():
+	pass
+
+
+# Called every frame. 'delta' is the elapsed time since the previous frame.
+func _process(delta):
+	pass
